a2d50708ef92151eee58a8ecba548d40fe5080fc
Галина Михайлова <galina89ruzhyk@mail.ru>
2021-12-10 19:08:07 +0800

    Created DeskController,DeskResourse,DeskStoreRequest, DeskListResource&&method store&&model Desk

---
+++ app/Http/Controllers/Api/DeskController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\DeskStoreRequest;
+use App\Http\Resources\DeskResource;
+use App\Models\Desk;
+use Illuminate\Http\Request;
+
+class DeskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index()
+    {
+        return DeskResource::collection(Desk::with('lists')->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return DeskResource
+     */
+    public function store(DeskStoreRequest $request)
+    {
+        $create_desk = Desk::create($request->validated());
+
+        return new DeskResource($create_desk);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return DeskResource
+     */
+    public function show($id)
+    {
+        return new DeskResource(Desk::with('lists')->findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+}
--- app/Models/Desk.php
+++ app/Models/Desk.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Desk extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
+
+    public  function lists(): hasMany
+    {
+        return $this->hasMany(DeskList::class);
+    }
 }
