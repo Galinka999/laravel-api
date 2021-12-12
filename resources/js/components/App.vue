c47585fc79d443e33789b41f55c90a663d14191c
Галина Михайлова <galina89ruzhyk@mail.ru>
2021-12-12 22:26:31 +0800

    Displaying desks using Axios.

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,9 +1,16 @@
 <template>
     <div class="container">
-        <p>
-            <router-link :to="{name: 'home'}">Главная</router-link>
-            <router-link :to="{name: 'about'}">О нас</router-link>
-        </p>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Task manager</a>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <router-link class="nav-link active" aria-current="page" :to="{name: 'home'}">Главная</router-link>
+                        <router-link class="nav-link  active" aria-current="page" :to="{name: 'desks'}">Доски</router-link>
+                    </div>
+                </div>
+            </div>
+        </nav>
         <div>
             <router-view></router-view>
         </div>
